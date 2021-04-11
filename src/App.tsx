9207325cc6a47b0f51a1ad4aaaa6172fa5c5fc79
Jihyun <jkim68888@gmail.com>
2021-04-11 13:13:43 +0900

    카운트 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './style.css';
 import IMAGE from './react.png';
 import LOGO from './logo.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
     return <>
         <h1>React TypeScript Webpack Study - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Logo" width="300" height="300" />
         <img src={LOGO} alt="Star Logo" width="300" height="300" />
+        <ClickCounter />
     </>
 }
